10de56fc2ef78f9e653f4688862ec23db1445d0a
Kenny K <koablood@gmail.com>
2023-02-17 03:00:39 -0800

    Linked to database

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,40 @@
+import supabase from "../config/supabaseClient"
+import { useEffect, useState } from 'react'
+
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null)
+  const [music, setMusic] = useState(null)
+
+  useEffect(() => {
+    const fetchMusic = async () => {
+      const { data, error } = await supabase
+        .from('music')
+        .select()
+      if (error) {
+        setFetchError('Could not fetch the music')
+        setMusic(null)
+      }
+      if (data) {
+        setMusic(data)
+        setFetchError(null)
+        console.log(data) // logs data in browsers inspect
+      }
+    }
+
+    fetchMusic()
+
+  }, [])
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && (<p>{fetchError}</p>)}
+      {music && (
+        <div className="music">
+          {music.map(music => (
+            <p>{music.title}</p>
+          ))}
+        </div>
+      )}
     </div>
   )
 }

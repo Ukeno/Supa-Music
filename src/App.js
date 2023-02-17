332eb0e73db37061a73f61a420b38c9b1ae477ad
Kenny K <koablood@gmail.com>
2023-02-17 00:31:03 -0800

    install supabase

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import Create from "./pages/Create"
 import Update from "./pages/Update"
-
+import Delete from "./pages/Delete"
 
 function App() {
   return (
@@ -12,12 +12,14 @@ function App() {
       <nav>
         <h1>Supa Music</h1>
         <Link to="/">Home</Link>
-        <Link to="/create">Create New Music</Link>
+        <input type="text" placeholder="Working on it.."></input>
+        <Link to="/create">Create</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/:id" element={<Update />} />
+        <Route path="/delete" element={<Delete />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Delete.js
@@ -0,0 +1,9 @@
+const Delete = () => {
+    return (
+      <div className="page create">
+        <h2>Delete</h2>
+      </div>
+    )
+  }
+  
+  export default Delete

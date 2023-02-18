88dc496ab2d27592ba479c0873b39f5b3c7650d2
Jared7Lu <109761254+Jared7Lu@users.noreply.github.com>
2023-02-17 21:27:19 -0800

    update folder layout, needs renaming

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,9 +1,14 @@
+import {Link} from 'react-router-dom'
+
 const MusicCard = ({ music }) => {
     return (
         <div className="music-card">
             <h3>{music.title}</h3>
             <p>{music.method}</p>
             <div className="rating">{music.rating}</div>
+            <Link to={'/' + music.id}>
+                <i className='material-icons'>edit</i>
+            </Link>
         </div>
     )
 }
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,7 +1,88 @@
+import { useEffect, useState } from "react"
+import { useParams, useNavigate } from 'react-router-dom'
+import supabase from "../config/supabaseClient"
+
 const Update = () => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState('')
+  const [formError, setFormError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!title || !method || !rating) {
+      setFormError('Please fill in all fields')
+      return
+    }
+
+    const {data, error} = await supabase
+    .from('music') 
+    .update([{ title, method, rating }])
+    .eq('id', id)
+    
+    if (error) {
+      setFormError('Please fill in all fields')
+    }
+    if (data) {
+      setFormError(null)
+      navigate('/')
+    }
+  }
+
+  useEffect(() => {
+    //need to rename !!!
+    const fetchComment = async () => {
+      const { data, error } = await supabase
+        .from('recipes')
+        .select()
+        .eq('id', id)
+        .single()
+
+      if (error) {
+        navigate('/', { replace: true })
+      }
+      if (data) {
+        setTitle(data.title)
+        setMethod(data.method)
+        setRating(data.rating)
+      }
+    }
+
+    fetchComment()
+  }, [id, navigate])
+
   return (
     <div className="page update">
-      <h2>Update</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Name</label>
+        <input 
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Comment</label>
+        <textarea
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating</label>
+        <input 
+          type="number"
+          id="title"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Update Comment</button>
+        {formError && <p className='error'>{formError}</p>}
+      </form>
     </div>
   )
 }
